90177f350f4c80d87cd702a5c14cb30c09db78e1
Nirina <nirina.rajaofetra@gmail.com>
2024-04-19 10:08:28 +0300

    mis à jour main

--- back_matagri_projet_3/src/main/java/mg/inclusiv/cdan8/projet3/Projet3Application.java
+++ back_matagri_projet_3/src/main/java/mg/inclusiv/cdan8/projet3/Projet3Application.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Projet3Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Projet3Application.class, args);
 	}
