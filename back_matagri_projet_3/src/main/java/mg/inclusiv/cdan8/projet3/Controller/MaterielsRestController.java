6ff3038b213c3f392227356bc0bda512d4e7aae1
Nirina <nirina.rajaofetra@gmail.com>
2024-04-22 17:11:41 +0300

    ajout produit (back)

---
+++ back_matagri_projet_3/src/main/java/mg/inclusiv/cdan8/projet3/Controller/MaterielsRestController.java
@@ -0,0 +1,26 @@
+package mg.inclusiv.cdan8.projet3.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import mg.inclusiv.cdan8.projet3.Entities.Materiels;
+import mg.inclusiv.cdan8.projet3.Servicies.MaterielsService;
+
+@RestController
+@RequestMapping("/api/materiels")
+public class MaterielsRestController {
+
+    @Autowired
+    private MaterielsService materielsService;
+
+    @PostMapping("/ajouter")
+    public ResponseEntity<Materiels> ajouterMateriel(@RequestBody Materiels materiel) {
+        Materiels nouveauMateriel = materielsService.addMateriel(materiel);
+        return new ResponseEntity<>(nouveauMateriel, HttpStatus.CREATED);
+    }
+    
+}
