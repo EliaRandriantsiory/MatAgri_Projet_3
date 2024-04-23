bddc57241621f1b6033a5c1f3c527303df79c54c
EliaRandriantsiory <elia.randriantsiory@gmail.com>
2024-04-23 12:51:13 +0300

    mis à jour session

--- front_matagri_projet_3/src/pages/Dashboard/Fournisseur/navbar.jsx
+++ front_matagri_projet_3/src/pages/Dashboard/Fournisseur/navbar.jsx
@@ -2,41 +2,62 @@ import { useEffect, useState } from "react";
 import { Link, Navigate, useNavigate } from "react-router-dom";
 import axios from "axios";
 function Navigation() {
-  const navigate = useNavigate();
-  const [currentMailUSer, setCurrentMailUser] = useState();
-  const [currentProfilUSer, setCurrentProfilUser] = useState({});
-  const handleOnClickLogout = (event) => {
-    setCurrentProfilUser({})
-    Navigate("/home");
-  };
 
-  useEffect(() =>{
-    setCurrentProfilUser({
-      idUser: 3,
-      name: null,
-      lastname: null,
-      address: null,
-      nif: null,
-      stat: null,
-      cin: null,
-      email: "rakoto@gmail.com",
-      region: null,
-      companyName: null,
-      password: "rakoto",
-      profile: {
-        idprofile: 1,
-        profile: "agriculteur",
-        roles: [],
-      },
-    });
-    if (Object.keys(currentProfilUSer).length === 0) {
-      console.log("Bonjour");
-    } else {
-      navigate("/home");
-      console.log("Erreur");
+  // const user={
+  //   idUser: 3,
+  //   name: null,
+  //   lastname: null,
+  //   address: null,
+  //   nif: null,
+  //   stat: null,
+  //   cin: null,
+  //   email: "rakoto@gmail.com",
+  //   region: null,
+  //   companyName: null,
+  //   password: "rakoto",
+  //   profile: {
+  //     idprofile: 1,
+  //     profile: "agriculteur",
+  //     roles: [],
+  //   },
+  // }
+  const [currentProfilUSer, setCurrentProfilUser] = useState({});
+  useEffect(() => {
+    axios.post('http://localhost:8082/api/home/authentification',{
+      email:"rakoto@gmail.com",
+      password:"rakoton"
     }
 
+    )
+  .then((response) => {
+    // Récupérer les données renvoyées par le backend
+    // setCurrentUser(response.data);
+     setCurrentProfilUser({...response.data}) 
+    
+  })
+  .catch((error) => {
+    // Gérer les erreurs de la requête
+    console.error(error);
+  });
+  
   },[])
+ 
+  const navigate = useNavigate();
+  
+  // const handleOnClickLogout = (event) => {
+  //   currentProfilUSer({})
+  //   Navigate("/home");
+  // };
+  // setCurrentProfilUser({...user})
+    console.log(currentProfilUSer)
+    if (Object.keys(currentProfilUSer).length !== 0) {
+        console.log("Bonjour");
+      } else {
+        navigate("/home");
+        console.log("Erreur");
+      }
+
+  
   
   // console.log(currentProfilUSer);
   return (
@@ -53,7 +74,7 @@ function Navigation() {
           <div className="profile-detail">
             <h5>Fashion Store</h5>
             <h6>750 followers | 10 review</h6>
-            <h6>{currentMailUSer}</h6>
+            <h6>{currentProfilUSer.email}</h6>
           </div>
         </div>
         <div className="faq-tab">
