88fa424e96342f99da218b83d2800fc8ca10029f
Rosotiana <rstcdan8@gmail.com>
2024-04-24 09:23:38 +0200

    correct

--- back_matagri_projet_3/src/main/java/mg/inclusiv/cdan8/projet3/Entities/Profiles.java
+++ back_matagri_projet_3/src/main/java/mg/inclusiv/cdan8/projet3/Entities/Profiles.java
@@ -27,10 +27,5 @@ public class Profiles {
     private String profile;
     @OneToMany(mappedBy = "profile")
     private List<Roles> roles;
-<<<<<<< HEAD
 
-=======
-    
-    
->>>>>>> rosotiana
 }
