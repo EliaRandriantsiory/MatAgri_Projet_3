27b5c62df0b2812ddb1078d201de27e2ec6c9e45
EliaRandriantsiory <elia.randriantsiory@gmail.com>
2024-04-27 22:30:12 +0300

    update materiels entities

--- back_matagri_projet_3/src/main/java/mg/inclusiv/cdan8/projet3/Entities/Materiels.java
+++ back_matagri_projet_3/src/main/java/mg/inclusiv/cdan8/projet3/Entities/Materiels.java
@@ -1,5 +1,6 @@
 package mg.inclusiv.cdan8.projet3.Entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -7,6 +8,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -23,10 +25,12 @@ public class Materiels {
     private String nomMat;
     private double PrixMAt;
     private int stockMat;
+    @Column(columnDefinition = "TEXT")
     private String descriptionMat;
+    @Column(columnDefinition = "TEXT")
     private String techniqueMat;
+    @Column(nullable = false) 
     private String imagePath;
-    private String imageDetailsPath;
 
     // @ManyToOne
     // @JoinColumn(name = "id_user")
