95752c533b5975a93e48480030ce11dbe539adf3
gitArnel456 <arnelmihaja@gmail.com>
2024-04-28 18:06:48 +0300

    REservation et detail pannier

---
+++ back_matagri_projet_3/src/main/java/mg/inclusiv/cdan8/projet3/Repositories/ReservationRepository.java
@@ -0,0 +1,14 @@
+
+package mg.inclusiv.cdan8.projet3.Repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import mg.inclusiv.cdan8.projet3.Entities.Reservation;
+
+
+public interface ReservationRepository extends JpaRepository<Reservation,Long> {
+
+      //List<Reservation> findAllByMateriel(Materiels materiels);
+      // List<Reservation> findByDateReservations(LocalDate dateDebut, LocalDate dateFin);
+
+}
