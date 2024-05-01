ba41d9b2cf007ef70cdb88d351a644647b2e5423
Rosotiana <rstcdan8@gmail.com>
2024-05-01 16:19:05 +0200

    back end

---
+++ back_matagri_projet_3/src/main/java/mg/inclusiv/cdan8/projet3/Controller/CommandeController.java
@@ -0,0 +1,40 @@
+
+package mg.inclusiv.cdan8.projet3.Controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import mg.inclusiv.cdan8.projet3.Entities.Commande;
+import mg.inclusiv.cdan8.projet3.Entities.Mutualisation;
+import mg.inclusiv.cdan8.projet3.Servicies.CommandeService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("/api/panier")
+public class CommandeController {
+    @Autowired
+    private CommandeService commandeService;
+
+    @GetMapping("/listePannier")
+    public List<Commande> detailPannier(){
+        List<Commande> DetailPannier = commandeService.getPannier();
+        return DetailPannier;
+    }
+
+    @PostMapping("/mutualisation")
+    public void mutualisation(@RequestBody Mutualisation mutualisation) {
+        
+        commandeService.mutualisation(mutualisation);
+    }
+    
+}
