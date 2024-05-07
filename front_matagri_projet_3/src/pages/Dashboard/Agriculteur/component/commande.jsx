27b2632f14ffb15ea6f478f3252d8c396af5e7ee
[Haritiana] <[rhartiana.fanomezana@mgail.com]>
2024-05-07 15:11:17 +0300

    row

--- front_matagri_projet_3/src/pages/Dashboard/Agriculteur/component/commande.jsx
+++ front_matagri_projet_3/src/pages/Dashboard/Agriculteur/component/commande.jsx
@@ -1,3 +1,5 @@
+import ListCommande from "./rowCommande";
+
 function Commande() {
     return ( <>
         <div className="tab-pane fade" id="orders">
@@ -12,214 +14,17 @@ function Commande() {
                             <table className="table cart-table order-table">
                               <thead>
                                 <tr className="table-head">
-                                  <th scope="col">image</th>
-                                  <th scope="col">Order Id</th>
+                                  <th scope="col">Designation</th>
+                                  <th scope="col">Quantité</th>
                                   <th scope="col">Détails du produit</th>
                                   <th scope="col">Statut</th>
-                                  <th scope="col">Prix</th>
+                                  <th scope="col">Prix journalier</th>
+                                  <th scope="col">Dates</th>
+                                  <th scope="col">Total</th>
                                   <th scope="col">Vue</th>
                                 </tr>
                               </thead>
-                              <tbody>
-                                <tr>
-                                  <td>
-                                    <a href="javascript:void(0)">
-                                      <img
-                                        src="../assets/images/pro3/1.jpg"
-                                        className="blur-up lazyloaded"
-                                        alt=""
-                                      />
-                                    </a>
-                                  </td>
-                                  <td>
-                                    <span className="mt-0">#125021</span>
-                                  </td>
-                                  <td>
-                                    <span className="fs-6">
-                                      Purple polo tshirt
-                                    </span>
-                                  </td>
-                                  <td>
-                                    <span className="badge rounded-pill bg-success custom-badge">
-                                      Shipped
-                                    </span>
-                                  </td>
-                                  <td>
-                                    <span className="theme-color fs-6">
-                                      $49.54
-                                    </span>
-                                  </td>
-                                  <td>
-                                    <a href="javascript:void(0)">
-                                      <i className="fa fa-eye text-theme" />
-                                    </a>
-                                  </td>
-                                </tr>
-                                <tr>
-                                  <td>
-                                    <a href="javascript:void(0)">
-                                      <img
-                                        src="../assets/images/pro3/2.jpg"
-                                        className="blur-up lazyloaded"
-                                        alt=""
-                                      />
-                                    </a>
-                                  </td>
-                                  <td>
-                                    <span className="mt-0">#125367</span>
-                                  </td>
-                                  <td>
-                                    <span className="fs-6">
-                                      Sleevless white top
-                                    </span>
-                                  </td>
-                                  <td>
-                                    <span className="badge rounded-pill bg-danger custom-badge">
-                                      Pending
-                                    </span>
-                                  </td>
-                                  <td>
-                                    <span className="theme-color fs-6">
-                                      $49.54
-                                    </span>
-                                  </td>
-                                  <td>
-                                    <a href="javascript:void(0)">
-                                      <i className="fa fa-eye text-theme" />
-                                    </a>
-                                  </td>
-                                </tr>
-                                <tr>
-                                  <td>
-                                    <a href="javascript:void(0)">
-                                      <img
-                                        src="../assets/images/pro3/27.jpg"
-                                        className="blur-up lazyloaded"
-                                        alt=""
-                                      />
-                                    </a>
-                                  </td>
-                                  <td>
-                                    <p>#125948</p>
-                                  </td>
-                                  <td>
-                                    <p className="fs-6">
-                                      multi color polo tshirt
-                                    </p>
-                                  </td>
-                                  <td>
-                                    <span className="badge rounded-pill bg-success custom-badge">
-                                      Shipped
-                                    </span>
-                                  </td>
-                                  <td>
-                                    <p className="theme-color fs-6">$49.54</p>
-                                  </td>
-                                  <td>
-                                    <a href="javascript:void(0)">
-                                      <i className="fa fa-eye text-theme" />
-                                    </a>
-                                  </td>
-                                </tr>
-                                <tr>
-                                  <td>
-                                    <a href="javascript:void(0)">
-                                      <img
-                                        src="../assets/images/pro3/28.jpg"
-                                        className="blur-up lazyloaded"
-                                        alt=""
-                                      />
-                                    </a>
-                                  </td>
-                                  <td>
-                                    <p>#127569</p>
-                                  </td>
-                                  <td>
-                                    <p className="fs-6">
-                                      Candy red solid tshirt
-                                    </p>
-                                  </td>
-                                  <td>
-                                    <span className="badge rounded-pill bg-success custom-badge">
-                                      Shipped
-                                    </span>
-                                  </td>
-                                  <td>
-                                    <p className="theme-color fs-6">$49.54</p>
-                                  </td>
-                                  <td>
-                                    <a href="javascript:void(0)">
-                                      <i className="fa fa-eye text-theme" />
-                                    </a>
-                                  </td>
-                                </tr>
-                                <tr>
-                                  <td>
-                                    <a href="javascript:void(0)">
-                                      <img
-                                        src="../assets/images/pro3/33.jpg"
-                                        className="blur-up lazyloaded"
-                                        alt=""
-                                      />
-                                    </a>
-                                  </td>
-                                  <td>
-                                    <p>#125753</p>
-                                  </td>
-                                  <td>
-                                    <p className="fs-6">
-                                      multicolored polo tshirt
-                                    </p>
-                                  </td>
-                                  <td>
-                                    <span className="badge rounded-pill bg-secondary custom-badge">
-                                      Canceled
-                                    </span>
-                                  </td>
-                                  <td>
-                                    <p className="theme-color fs-6">$49.54</p>
-                                  </td>
-                                  <td>
-                                    <a href="javascript:void(0)">
-                                      <i className="fa fa-eye text-theme" />
-                                    </a>
-                                  </td>
-                                </tr>
-                                <tr>
-                                  <td>
-                                    <a href="javascript:void(0)">
-                                      <img
-                                        src="../assets/images/pro3/34.jpg"
-                                        className="blur-up lazyloaded"
-                                        alt=""
-                                      />
-                                    </a>
-                                  </td>
-                                  <td>
-                                    <span>#125021</span>
-                                  </td>
-                                  <td>
-                                    <span className="fs-6">
-                                      Men's Sweatshirt
-                                    </span>
-                                  </td>
-                                  <td>
-                                    <span className="badge rounded-pill bg-secondary custom-badge">
-                                      Canceled
-                                    </span>
-                                  </td>
-                                  <td>
-                                    <span className="theme-color fs-6">
-                                      $49.54
-                                    </span>
-                                  </td>
-                                  <td>
-                                    <a href="javascript:void(0)">
-                                      <i className="fa fa-eye text-theme" />
-                                    </a>
-                                  </td>
-                                </tr>
-                              </tbody>
+                              <ListCommande/>
                             </table>
                           </div>
                         </div>
---
+++ front_matagri_projet_3/src/pages/Dashboard/Agriculteur/component/rowCommande.jsx
@@ -0,0 +1,50 @@
+function ListCommande() {
+    return ( 
+        <>
+            <tbody>
+                <tr>
+                    <td>
+                        <a href="javascript:void(0)">
+                            <img
+                                src="../assets/images/pro3/1.jpg"
+                                className="blur-up lazyloaded"
+                                alt=""
+                            />
+                        </a>
+                    </td>
+                    <td>
+                        <span className="mt-0">2</span>
+                    </td>
+                    <td>
+                        <span className="mt-0">2</span>
+                    </td>
+                    <td>
+                        <span className="mt-0">2</span>
+                    </td>
+                    <td>
+                        <span className="fs-6">
+                            Purple polo tshirt
+                        </span>
+                    </td>
+                    <td>
+                        <span className="badge rounded-pill bg-success custom-badge">
+                            Shipped
+                        </span>
+                    </td>
+                    <td>
+                        <span className="theme-color fs-6">
+                            Ar 49000.54
+                        </span>
+                    </td>
+                    <td>
+                        <a href="javascript:void(0)">
+                            <i className="fa fa-eye text-theme" />
+                        </a>
+                    </td>
+                </tr>
+            </tbody>
+        </>
+    );
+}
+
+export default ListCommande;
