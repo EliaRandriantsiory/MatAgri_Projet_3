a4aa488395fcc57200b3ae8802c1036ba861f19c
Nasandratriniaina <andrianavaloniarivo@gmail.com>
2024-05-07 17:12:59 +0300

    commit devis

--- front_matagri_projet_3/src/pages/DevisComponent.jsx
+++ front_matagri_projet_3/src/pages/DevisComponent.jsx
@@ -5,33 +5,34 @@ import html2canvas from "html2canvas";
 function DevisComponents() {
   const exportPDF = () => {
     const input = document.getElementById("pdf-content"); // Obtenez une référence à l'élément contenant le contenu à inclure dans le PDF
-
-    html2canvas(input).then((canvas) => {
-      const imgData = canvas.toDataURL("image/png");
-      const pdf = new jsPDF("p", "pt", "a4");
-
-      const width = pdf.internal.pageSize.getWidth();
-      const height = (canvas.height * width) / canvas.width;
-
-      pdf.addImage(imgData, "PNG", 0, 0, width, height);
-      pdf.save("demo.pdf");
+    const buttons = input.querySelectorAll(".buttons");
+    buttons.forEach((button) => {
+      button.style.display = "none";
     });
+    html2canvas(input)
+      .then((canvas) => {
+        const imgData = canvas.toDataURL("image/png");
+        const pdf = new jsPDF("p", "pt", "a4");
+
+        const width = pdf.internal.pageSize.getWidth();
+        const height = (canvas.height * width) / canvas.width;
+
+        pdf.addImage(imgData, "PNG", -200, -75, 1000, 800);
+        pdf.save("demo.pdf");
+        buttons.forEach((button) => {
+          button.style.display = "block";
+        });
+      })
+      .catch((err) => console.error(err));
   };
   return (
-    <div>
+    <div id="pdf-content">
       <section className="theme-invoice-1 section-b-space">
-        <div className="container" id="pdf-content">
+        <div className="container">
           <div className="row">
             <div className="col-xl-8 m-auto">
               <div className="invoice-wrapper">
                 <div className="invoice-header">
-                  <div className="upper-icon">
-                    <img
-                      src="../assets/images/invoice/invoice.svg"
-                      className="img-fluid"
-                      alt=""
-                    />
-                  </div>
                   <div className="row header-content">
                     <div className="col-md-6">
                       <img
@@ -40,70 +41,47 @@ function DevisComponents() {
                         alt=""
                       />
                     </div>
-                    <div className="col-md-6 text-md-end mt-md-0 mt-4">
-                      <h3>MATAgri</h3>
-                      <div className="mt-md-4 mt-3">
-                        <h4 className="mb-2">Antananarivo, 101</h4>
-                        <h4 className="mb-0">Faravohitra</h4>
-                      </div>
-                    </div>
                   </div>
                   <div className="detail-bottom">
-                    <table>
-                      <tr>
-                        <td>
-                          <table>
-                            <tr>
-                              <td>
-                                <h4>Fait le :</h4>
-                              </td>
-                              <td>
-                                <h4>12 mars 2024</h4>
-                              </td>
-                            </tr>
-                            <tr>
-                              <td>
-                                <h4>Nom :</h4>
-                              </td>
-                              <td>
-                                <h4>Kotobe</h4>
-                              </td>
-                            </tr>
-                          </table>
-                        </td>
-                        <td>
-                          <table>
-                            <tr>
-                              <td>
-                                <h4>Devis n° :</h4>
-                              </td>
-                              <td>
-                                <h4>0001</h4>
-                              </td>
-                            </tr>
-                            <tr>
-                              <td>
-                                <h4>email :</h4>
-                              </td>
-                              <td>
-                                <h4>user@gmail.com</h4>
-                              </td>
-                            </tr>
-                          </table>
-                        </td>
-                      </tr>
-                    </table>
+                    <div class="container">
+                      <div class="row">
+                        <div class="col-md-6">
+                          <h4>Faravohitra</h4>
+                          <h4>Antananarivo, 101</h4>
+                          <br />
+                          <h4>
+                            <b>Devis n°</b> : 2222
+                          </h4>
+                          <h4>Date : 08-05-2024</h4>
+                        </div>
+                        <div
+                          class="col-md-6 justify-content-md-end"
+                          style={{
+                            textAlign: "left",
+                            marginLeft: "600px",
+                            marginTop: "-107px",
+                          }}
+                        >
+                          <h4>Client : Bema</h4>
+                          <h4>Adresse : Ankorondrano</h4>
+                          <h4>Email : Users@gmail.com</h4>
+                        </div>
+                      </div>
+                    </div>
                   </div>
                 </div>
                 <div className="invoice-body table-responsive-md">
                   <table className="table table-borderless mb-0">
                     <thead>
                       <tr>
+                        <th style={{ textTransform: "none" }}>Designation</th>
                         <th style={{ textTransform: "none" }}>
-                          Nom du materiel
+                          Description détaillée
                         </th>
-                        <th style={{ textTransform: "none" }}>Description</th>
-                        <th style={{ textTransform: "none" }}>Prix</th>
+                        <th style={{ textTransform: "none" }}>Qté</th>
+                        <th style={{ textTransform: "none" }}>
+                          Prix Journalier
+                        </th>
                         <th style={{ textTransform: "none" }}>De</th>
                         <th style={{ textTransform: "none" }}>A</th>
                         <th style={{ textTransform: "none" }}>Montant</th>
@@ -113,6 +91,7 @@ function DevisComponents() {
                       <tr>
                         <td>kubota</td>
                         <td>kubota 220cv</td>
+                        <td>1</td>
                         <td>120 000 Ar</td>
                         <td>12-03-2024</td>
                         <td>07-04-2024</td>
@@ -121,6 +100,7 @@ function DevisComponents() {
                       <tr>
                         <td>kubota</td>
                         <td>kubota 220cv</td>
+                        <td>1</td>
                         <td>120 000 Ar</td>
                         <td>12-03-2024</td>
                         <td>07-04-2024</td>
@@ -129,6 +109,7 @@ function DevisComponents() {
                       <tr>
                         <td>kubota</td>
                         <td>kubota 220cv</td>
+                        <td>1</td>
                         <td>120 000 Ar</td>
                         <td>12-03-2024</td>
                         <td>07-04-2024</td>
@@ -137,6 +118,7 @@ function DevisComponents() {
                       <tr>
                         <td>kubota</td>
                         <td>kubota 220cv</td>
+                        <td>1</td>
                         <td>120 000 Ar</td>
                         <td>12-03-2024</td>
                         <td>07-04-2024</td>
@@ -156,7 +138,7 @@ function DevisComponents() {
                               marginRight: "50px",
                             }}
                           >
-                            PRIX TOTAL :
+                            Frais de Livraison :
                           </label>
                         </td>
                         <td>
@@ -166,7 +148,29 @@ function DevisComponents() {
                               marginRight: "60px",
                             }}
                           >
-                            320 080 Ar
+                            20 000 Ar
+                          </label>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>
+                          <label
+                            style={{
+                              fontWeight: "bold",
+                              marginRight: "50px",
+                            }}
+                          >
+                            TOTAL A PAYER :
+                          </label>
+                        </td>
+                        <td>
+                          <label
+                            style={{
+                              textAlign: "left",
+                              marginRight: "60px",
+                            }}
+                          >
+                            320 000 Ar
                           </label>
                         </td>
                       </tr>
@@ -174,33 +178,22 @@ function DevisComponents() {
                   </table>
                 </div>
                 <div style={{ textAlign: "center" }}>
-                  <label style={{ fontWeight: "bold", marginRight: "25px" }}>
-                    Arrêté le présent devis à la somme de :
+                  <label style={{ marginRight: "392px" }}>
+                    <b>Arrêté le présent devis à la somme de :</b>{" "}
+                    {NombreToLetter(320000)} Ariary
                   </label>
-                  <label
-                    style={{
-                      textAlign: "left",
-                      marginRight: "25px",
-                    }}
-                  >
-                    {NombreToLetter(320080)} Ariary
+                  <label style={{ marginRight: "670px" }}>
+                    Mode de Paiement : CB
+                  </label>
+                  <label style={{ marginRight: "710px" }}>
+                    Validité : 15 jours
                   </label>
                 </div>
-
+                <br />
                 <div className="invoice-footer text-end">
                   <div className="buttons">
-                    <a
-                      href="#"
-                      className="btn black-btn btn-solid me-2"
-                      onclick="window.print();"
-                    >
-                      Exporter en PDF
-                    </a>
-                    <button
-                      className="btn btn-solid rounded-2"
-                      onClick={exportPDF}
-                    >
-                      Imprimer
+                    <button className="btn btn-solid" onClick={exportPDF}>
+                      Télécharger
                     </button>
                   </div>
                 </div>
