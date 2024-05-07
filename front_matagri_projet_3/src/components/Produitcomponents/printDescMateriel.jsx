f1c83c7d6dc3e6c3af14023a48fa3f9588ed762f
[Haritiana] <[rhartiana.fanomezana@mgail.com]>
2024-05-07 12:24:33 +0300

    integration

--- front_matagri_projet_3/src/components/Produitcomponents/printDescMateriel.jsx
+++ front_matagri_projet_3/src/components/Produitcomponents/printDescMateriel.jsx
@@ -1,24 +1,15 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
-import { Modal, Button } from "react-bootstrap";
-import CalendarTodayIcon from "@mui/icons-material/CalendarToday";
-import CheckIcon from "@mui/icons-material/Check";
-import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
-import { DateRange, DateRangePicker } from "react-date-range";
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import React, { useState } from "react";
+import { Button, Modal } from "react-bootstrap";
 import ReserverPanier from "../../pages/Panier/reserverAddPanier";
 import PrintDetailTechMat from "../textComponent/printDescTechMateriel";
 import PrintPrixUser from "../textComponent/printPrixUser";
-import SaisieAutomatiqueVille from "../textComponent/testsaisiautomatique";
-
-
-function AjoutPanier({ materialItem,setPanierMat }) {
-  const [quantityPanier, setQuantity] = useState(1);
-
+function AjoutPanier({ materialItem }) {
+  const [quantity, setQuantity] = useState(1);
   const [distance, setDistance] = useState("");
   const [listPanierMat, setListPanierMat] = useState([]);
-  // const [panierMAt, setPanierMat] = useState({});
-  const [startDateCrenau, setStartDateCrenau] = useState()
-  const [endDateCrenau, setEndDateCrenau] = useState()
+  const [panierMAt, setPanierMat] = useState({});
+
   const [notif, setNotif] = useState();
 
   const [lieuExploitation, setLieuExploitation] = useState("");
@@ -49,14 +40,14 @@ function AjoutPanier({ materialItem,setPanierMat }) {
 
   const incrementQuantity = () => {
     setQuantity((prevQuantity) => prevQuantity + 1);
-
   };
   const decrementQuantity = () => {
-    if (quantityPanier > 1) {
+    if (quantity > 1) {
       setQuantity((prevQuantity) => prevQuantity - 1);
     }
   };
 
+  
   const handleOnClickAddCard = (event) => {
     setPanierMat({
       materiel: {
@@ -71,40 +62,16 @@ function AjoutPanier({ materialItem,setPanierMat }) {
         prixMAt: materialItem.prixMAt,
       },
       users: {},
-      quantity: quantityPanier,
-      startDate: startDateCrenau,
-      endDate: endDateCrenau,
+      quantity: 2,
+      startDate: "15/11/2024",
+      endDate: "25/11/2024",
     });
-
   };
-
   const handleValidationClick = async () => {
-    try {
-      const response = await axios.post(
-        `http://localhost:8082/distance/calculate/${materialItem?.materielId}`,
-        {
-          destination: lieuExploitation,
-        }
-      );
-      if (response.status === 200) {
-        setDistance(response.data);
-        return response.data;
-      }
-    } catch (error) {
-      return "Une erreur s'est produite lors du calcul de la distance";
-    }
-    // handleCloseDescMat()
-
+    handleCloseDescMat()
+    
   };
-
-  useEffect(() => {
-    if (lieuExploitation === "") {
-      setDistance("");
-    }
-  }, [lieuExploitation]);
-
 // console.log(materialItem)
-
   return (
     <div>
       <a onClick={handleOpenCalendar}>
@@ -127,14 +94,7 @@ function AjoutPanier({ materialItem,setPanierMat }) {
             className="row"
             style={{ display: "flex", flexDirection: "column" }}
           >
-            <div
-              style={{
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-                flexDirection: "row",
-              }}
-            >
+            <div style={{display: "flex",alignItems:"center" , justifyContent:"center", flexDirection: "row"}} >
               <img
                 src={`${process.env.PUBLIC_URL}/assets/images/materiels/${
                   JSON.parse(materialItem.imagePath)[0]
@@ -143,42 +103,30 @@ function AjoutPanier({ materialItem,setPanierMat }) {
                 className="img-fluid blur-up lazyload bg-img"
               />
             </div>
-            <br />
+<br/>
             <div className="product-right">
-              <h2>
-                <b>{materialItem.nomMat}</b>
-              </h2>
+            <h2><b>{materialItem.nomMat}</b></h2>
               <h2 className="product-title">Description</h2>
-              <p>{materialItem.descriptionMat}</p>
-              <br />
+                <p>{materialItem.descriptionMat}</p>
+                <br/>
+                <h2 className="product-title">Description technique matériel</h2>
+                {/* <p>{materialItem.techniqueMat}</p>
+                 */}
+                 <PrintDetailTechMat desctechMat={materialItem.techniqueMat} />
+                <br/>
+                <h2 className="product-title">Taux journalière : <PrintPrixUser TextPrix={materialItem.prixMAt} monnai={"MLG"} /></h2>
+                <br/>
 
-              {/* <h2 className="product-title">Description technique matériel</h2> */}
-              {/* <p>{materialItem.techniqueMat}</p>
+                <div className="border-product">
               
-              <PrintDetailTechMat desctechMat={materialItem.techniqueMat} />
+              <div>
+              <label hidden className=""> Entrer votre plage de date : </label>
+              <ReserverPanier />
+              </div>
               <br />
-              <h2 className="product-title">
-                Taux journalière :{" "}
-                <PrintPrixUser TextPrix={materialItem.prixMAt} monnai={"MLG"} />
-              </h2>
-              <br />
-
-              <div className="border-product">
-                <div>
-                  <label hidden className="">
-                    {" "}
-                    Entrer votre plage de date :{" "}
-                  </label>
-                  <ReserverPanier setStartDateCrenau={setStartDateCrenau} setEndDateCrenau={setEndDateCrenau}  /> */}
-
-
-
-
-
-
               <div className="d-flex align-items-start"  >
                 <label className="d-block mb-2" >
-                {/* style={{display: "flex",alignItems:"center" , justifyContent:"center", flexDirection: "row"}} */}
+                {/* style={{display: "flex",alignItems:"center" , justifyContent:"center", flexDirPriion: "row"}} */}
                   Entrer votre lieu d'exploitation&nbsp;&nbsp;
                 </label>
                 <div className="d-flex">
@@ -207,75 +155,13 @@ function AjoutPanier({ materialItem,setPanierMat }) {
                   >
                     Valider
                   </button>
+                </div>
+              </div>
+              <label className="d-block mb-2">
+                Votre distance est de :{distance ? distance : ""}
+              </label>
 
-                </div>
-                <br />
-                <div className="d-flex align-items-start">
-                  <label className="d-block mb-2">
-                    {/* style={{display: "flex",alignItems:"center" , justifyContent:"center", flexDirection: "row"}} */}
-                    Entrer votre lieu d'exploitation :
-                  </label>
-                  <div className="d-flex">
-                    {/* <SaisieAutomatiqueVille /> */}
-                    <input
-                      type="text"
-                      className="form-control"
-                      placeholder="Lieu d'exploitation"
-                      style={{ opacity: 0.7, fontSize: "0.9rem" }}
-                      value={lieuExploitation}
-                      onChange={handleLieuExploitationChange}
-                    />
-                    <button
-                      className="btn btn-solid"
-                      disabled={
-                        lieuExploitation === "" || lieuExploitation.length === 0
-                          ? true
-                          : false
-                      }
-                      onClick={() => handleValidationClick()}
-                      style={{ border: "none" }}
-                    >
-                      Valider
-                    </button>
-                  </div>
-                </div>
-                <label className="d-block mb-2">
-                  Votre distance est de :{distance ? distance : ""}
-                </label>
-
-                <div className="product-description border-product">
-                  <h6 className="product-title">Quantité</h6>
-                  <div className="qty-box">
-                    <div className="input-group">
-                      <span className="input-group-prepend">
-                        <button
-                          type="button"
-                          className="btn quantity-left-minus"
-                          onClick={decrementQuantity}
-                        >
-                          <i className="ti-angle-left" />
-                        </button>{" "}
-                      </span>
-                      <input
-                        type="text"
-                        name="quantity"
-                        className="form-control input-number"
-                        value={quantityPanier}
-                        defaultValue={1}
-                        readOnly
-                      />{" "}
-                      <span className="input-group-prepend">
-                        <button
-                          type="button"
-                          className="btn quantity-right-plus"
-                          onClick={incrementQuantity}
-                        >
-                          <i className="ti-angle-right" />
-                        </button>
-                      </span>
-                    </div>
-                  </div>
-                </div>
+              <br />
               </div>
               {/* <div className="product-buttons">
                     <button
@@ -286,6 +172,8 @@ function AjoutPanier({ materialItem,setPanierMat }) {
                     </button>
                   </div> */}
             </div>
+
+            
           </div>
         </Modal.Body>
         <Modal.Footer>
